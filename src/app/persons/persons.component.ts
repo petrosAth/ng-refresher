b5794807ac35f8ae7f1f973184ef090cc8c43230
Petros Athanasoulis <develop@athanasoulis.me>
2024-06-12 17:33:37 +0300

    Pushing Data Around with Subjects

---
+++ src/app/persons/persons.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { PersonsService } from './persons.service';
+
+@Component({
+  selector: 'app-persons',
+  templateUrl: './persons.component.html',
+})
+export class PersonsComponent implements OnInit, OnDestroy {
+  personList: string[];
+  private personListSubs: Subscription;
+
+  constructor(private prsService: PersonsService) {}
+
+  ngOnInit(): void {
+    this.personList = this.prsService.persons;
+    this.personListSubs = this.prsService.personsChanged.subscribe(
+      (persons) => {
+        this.personList = persons;
+      }
+    );
+  }
+
+  onRemovePerson(personName: string): void {
+    this.prsService.removePerson(personName);
+  }
+
+  ngOnDestroy() {
+    this.personListSubs.unsubscribe();
+  }
+}
